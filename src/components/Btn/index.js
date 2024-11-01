64a7d9a8f0069d42715b6f3d8a660ef4af72191e
Rudy Frassin <rudy.frassin@gmail.com>
2024-11-01 12:14:56 +0100

    Btn component Android 10

--- src/components/Btn/index.js
+++ src/components/Btn/index.js
@@ -62,7 +62,7 @@ const Btn = ({
                               setDynamicLink('https://play.google.com/store');
                         } else {
                               // Scenario 2: Navigateur classique Android
-                              setDynamicLink('market://');
+                              setDynamicLink('market://search?q=');
                         }
                   } else {
                         // Autres systèmes d'exploitation (Windows, Linux, etc.)
